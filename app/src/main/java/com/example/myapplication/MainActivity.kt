1d39f98d27232d612570dda50e921187ed3951fc
aksh-sush <techporcupines@gmail.com>
2026-02-05 21:00:07 +0530

    first commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.myapplication
+import android.graphics.Color
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        val start=findViewById<Button>(R.id.starter)
+        start.setOnClickListener { startActivity(android.content.Intent(this, exp1_gui::class.java)) } //context, target class
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/myapplication/exp1_gui.kt
@@ -0,0 +1,38 @@
+package com.example.myapplication
+
+import android.graphics.Color
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class exp1_gui: AppCompatActivity() { //inheretance
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.exp1_gui)
+        val fontsize: Button = findViewById(R.id.button1)
+        val font_colour: Button = findViewById(R.id.button2)
+        val backgroundcolour: Button = findViewById(R.id.button3)
+        var i=0
+    fontsize.setOnClickListener {
+        fontsize.textSize = fontsize.textSize + 5
+
+    }
+    font_colour.setOnClickListener {
+        when (i){
+            0->font_colour.setTextColor(Color.RED)
+            1->font_colour.setTextColor(Color.BLUE)
+            2->font_colour.setTextColor(Color.GREEN)
+        }
+        i=(i+1)%3
+
+    }
+    backgroundcolour.setOnClickListener {
+        when (i){
+            0->backgroundcolour.setBackgroundColor(Color.RED)
+            1->backgroundcolour.setBackgroundColor(Color.BLUE)
+            2->backgroundcolour.setBackgroundColor(Color.GREEN)
+        }
+        i=(i+1)%3
+    }
+
+}}
